e3e10ec6f68071e49cc32498bc393653a9f8520b
Dzmitry Shchepin <Dzmitry.Shchepin@gmail.com>
2024-02-26 17:00:55 +0300

    Isomorphic Strings

---
+++ src/main/java/com/akabynga/leetcode/IsomorphicStrings.java
@@ -0,0 +1,33 @@
+package com.akabynga.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class IsomorphicStrings {
+
+    public static void main(String[] args) {
+        System.out.println(isIsomorphic("foo", "bar"));
+    }
+
+    public static boolean isIsomorphic(String s, String t) {
+
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        int[] sToT = new int[128];
+        int[] tToS = new int[128];
+
+        for (int i = 0; i < t.length(); i++) {
+            if (sToT[t.charAt(i)] != tToS[s.charAt(i)]) {
+                return false;
+            }
+
+            sToT[t.charAt(i)] = i + 1;
+            tToS[s.charAt(i)] = i + 1;
+
+        }
+
+        return true;
+    }
+}
