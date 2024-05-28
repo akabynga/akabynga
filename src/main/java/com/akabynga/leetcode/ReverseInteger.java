0849781a0c65cd71dd5cb773b71b49c331a8a2bb
Shchepin Dzmitry <Dzmitry.Shchepin@gmail.com>
2024-05-28 11:58:13 +0300

    7. Reverse Integer - adjustments

--- src/main/java/com/akabynga/leetcode/ReverseInteger.java
+++ src/main/java/com/akabynga/leetcode/ReverseInteger.java
@@ -8,14 +8,18 @@ public class ReverseInteger {
     }
 
     public static int reverse(int x) {
-
+        boolean negative = x < 0;
         int result = 0;
+        x = Math.abs(x);
+
         while (x != 0) {
             int lastDigit = x % 10;
-            result = result * 10 + lastDigit;
+            if (result > (Integer.MAX_VALUE - lastDigit) / 10) {
+                result = result * 10 + lastDigit;
+            }
             x /= 10;
         }
-        return result;
+        return negative ? -result : result;
     }
 
 }
